0503060eecee0bed3910768f8855cd945b160126
mariekarpinska <karpinska@umail.ucsb.edu>
2023-10-16 14:51:43 -0700

    initial app

--- giftGivingApp/App.js
+++ giftGivingApp/App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
+import AddGiftScreen from "./src/screens/AddGiftScreen";
+import CalendarScreen from "./src/screens/CalendarScreen";
 
 const Stack = createStackNavigator();
 
@@ -23,6 +25,8 @@ export default function App() {
         <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Add Gift" component={AddGiftScreen} />
+          <Stack.Screen name="Calendar" component={CalendarScreen} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
--- giftGivingApp/src/screens/HomeScreen.js
+++ giftGivingApp/src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.headerText}>Home Screen</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Add Gift")}
+        >
+          <Text style={styles.buttonText}>Go to Add Gift Page</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Calendar")}
+        >
+          <Text style={styles.buttonText}>Go to Calendar Page</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,8 +26,28 @@ function HomeScreen() {
 const styles = {
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  headerText: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    padding: 10,
+    width: "100%",
+    marginVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
   },
 };
 
