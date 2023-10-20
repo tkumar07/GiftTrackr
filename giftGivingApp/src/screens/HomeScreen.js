6db877ff83f421748a4997ec42e367a73929b13d
Archit Gupta <40641696+archit-gpt@users.noreply.github.com>
2023-10-19 20:16:24 -0700

    Modify HomeScreen to manage the budget data in the state

--- giftGivingApp/src/screens/HomeScreen.js
+++ giftGivingApp/src/screens/HomeScreen.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import BudgetSection from "../components/BudgetSection";  // Import the BudgetSection
 
 function HomeScreen({ navigation }) {
+  const [totalBudget, setTotalBudget] = useState(1000);  // Default budget
+  const [amountSpent, setAmountSpent] = useState(0);
+
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Home Screen</Text>
+      
+      {/* Display Budget Section */}
+      <BudgetSection totalBudget={totalBudget} amountSpent={amountSpent} />
+
+      <Text>Set Your Budget:</Text>
+      <TextInput 
+        style={styles.input} 
+        value={totalBudget.toString()} 
+        onChangeText={(value) => setTotalBudget(Number(value))}
+        keyboardType="numeric"
+      />
+
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
