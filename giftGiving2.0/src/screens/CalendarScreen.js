0a3c1e1f044aa6319a60ca99b7d55a3d62d07607
tkumar07 <72886906+tkumar07@users.noreply.github.com>
2023-10-26 21:12:43 -0700

    Update CalendarScreen.js w/formatting, dots for dates

--- giftGiving2.0/src/screens/CalendarScreen.js
+++ giftGiving2.0/src/screens/CalendarScreen.js
@@ -1,13 +1,66 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Calendar } from "react-native-calendars";
 
 const CalendarScreen = () => {
+  const [markedDates, setMarkedDates] = useState({});
+  const [selectedDate, setSelectedDate] = useState(null);
+  const dotColor = "blue"; // You can specify your desired dot color
+
+  // Function to toggle the dot on the selected date
+  const toggleDotOnSelectedDate = () => {
+    if (selectedDate) {
+      if (markedDates[selectedDate]) {
+        // The selected date already has a dot, so remove it
+        const { [selectedDate]: removedDot, ...remainingDates } = markedDates;
+        setMarkedDates(remainingDates);
+      } else {
+        // The selected date does not have a dot, so add one
+        setMarkedDates({
+          ...markedDates,
+          [selectedDate]: {
+            selected: true,
+            marked: true,
+            dotColor,
+          },
+        });
+      }
+    }
+  };
+
+  // Handle date press and set the selectedDate
+  const handleDayPress = (day) => {
+    setSelectedDate(day.dateString);
+    toggleDotOnSelectedDate();
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Calendar />
+    <View style={styles.container}>
+      <Text style={styles.pageHeader}>Calendar</Text>
+      <View style={styles.calendarContainer}>
+        <Calendar
+          markedDates={markedDates}
+          onDayPress={handleDayPress}
+        />
+      </View>
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  pageHeader: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  calendarContainer: {
+    paddingVertical: 20,
+  },
+});
 
 export default CalendarScreen;
