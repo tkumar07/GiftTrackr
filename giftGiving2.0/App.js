b1b3f8777ab271be975aa59acdbf56dd5f3b6fb1
Shang-Hsun Yang <shanghsunyang@169-231-23-59.wireless.ucsb.edu>
2023-10-26 18:02:49 -0700

    Revert "User can input the gift data now. The gift information can be stored locally now."
    This reverts commit b46ffd2b17e37c9c2e0c75a583dd16b57ac7f948.

--- giftGiving2.0/App.js
+++ giftGiving2.0/App.js
@@ -1,18 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState, useEffect} from "react";
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Home from "./Home";
 import CalendarScreen from "./src/screens/CalendarScreen";
 import BudgetScreen from "./src/screens/BudgetScreen";
 import AddGift from "./src/screens/AddGift";
-import React, { Component } from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
+import { collection, onSnapshot } from "firebase/firestore"
+import { db } from "./src/config/firebase"
+import Login from "./src/screens/Login"
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [people, setPeople] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    setLoading(true)
+    const usersQuery = collection(db, "users")
+    onSnapshot(usersQuery, (snapshot) => {
+      let usersList = []
+      snapshot.docs.map((doc) => usersList.push({ ...doc.data(), id: doc.id }))
+      setPeople(usersList)
+      setLoading(false)
+    })
+  }, [])
+
+  const renderItem = ({ item }) => (
+    <View style={{ marginTop: 10 }}>
+      <Text>{item.username}</Text>    
+      <Text>{item.password}</Text>    
+    </View>
+  )
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 50,
+    },
+  })
+
   return (
     <NavigationContainer>
+      {!isLoggedIn ? (
+        <Login onSuccessfulLogin={() => setIsLoggedIn(true)} />
+      ) : (
+        <>
       <Tab.Navigator
         labeled={false}
         barStyle={{ backgroundColor: "black" }}
@@ -59,6 +98,8 @@ export default function App() {
           }}
         />
       </Tab.Navigator>
+        </>
+      )}
     </NavigationContainer>
   );
 }
--- giftGiving2.0/Home.js
+++ giftGiving2.0/Home.js
@@ -1,78 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, Alert } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 import { GiftDetailsCard } from "./src/components/GiftDetailsCard";
 import { styles } from "./src/styles";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function HomeScreen({ navigation }) {
-  const [giftCards, setGiftCards] = useState([]);
+const fakeGiftCards = [
+  {
+    recipient: "Alice",
+    date: "November 11, 2023",
+    occasion: "Birthday",
+    budget: "$50",
+    likes: "Disney, Mandalorian, Painting",
+    dislikes: "Coffee",
+    decidedGift: "grogu stuffed animal",
+  },
+  {
+    recipient: "Bob",
+    date: "December 16, 2023",
+    occasion: "Anniversary",
+    budget: "$75",
+    likes: "Movies, Painting, Cooking",
+    dislikes: "Technology, Sports",
+    decidedGift: "undecided",
+  },
+];
 
-  useEffect(() => {
-    // Fetch the gift card data from AsyncStorage when the component mounts
-    async function fetchGiftCards() {
-      const savedGiftData = await AsyncStorage.getItem('giftData');
-      if (savedGiftData) {
-        const parsedGiftData = JSON.parse(savedGiftData);
-        setGiftCards(parsedGiftData);
-      }
-    }
-    fetchGiftCards();
-  }, []);
-
-  const handleDelete = async (index) => {
-    // Display a confirmation alert before deleting
-    Alert.alert(
-      "Delete Gift",
-      "Are you sure you want to delete this gift?",
-      [
-        {
-          text: "No",
-          style: "cancel",
-        },
-        {
-          text: "Yes",
-          onPress: async () => {
-            // Remove the selected gift card from the state
-            const updatedGiftCards = [...giftCards];
-            updatedGiftCards.splice(index, 1);
-            setGiftCards(updatedGiftCards);
-
-            // Update the data in AsyncStorage
-            await AsyncStorage.setItem('giftData', JSON.stringify(updatedGiftCards));
-          },
-        },
-      ]
-    );
-  };
-
+function HomeScreen() {
   return (
     <View style={{ ...styles.container, marginTop: 10 }}>
       <Text style={styles.pageHeader}>Upcoming Gifts:</Text>
-      {giftCards.map((giftCard, index) => (
-        <View key={index}>
-          <GiftDetailsCard
-            recipient={giftCard.recipient}
-            date={giftCard.date}
-            occasion={giftCard.occasion}
-            budget={giftCard.budget}
-            likes={giftCard.likes}
-            dislikes={giftCard.dislikes}
-            decidedGift={giftCard.decidedGift}
-          />
-          <TouchableOpacity
-            style={styles.deleteButton}
-            onPress={() => handleDelete(index)}
-          >
-            <Text style={styles.deleteButtonText}>Delete</Text>
-          </TouchableOpacity>
-        </View>
+      {fakeGiftCards.map((giftCard, index) => (
+        <GiftDetailsCard
+          key={index}
+          recipient={giftCard.recipient}
+          date={giftCard.date}
+          occasion={giftCard.occasion}
+          budget={giftCard.budget}
+          likes={giftCard.likes}
+          dislikes={giftCard.dislikes}
+          decidedGift={giftCard.decidedGift}
+        />
       ))}
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => navigation.navigate('AddGift')}
-      >
-        <Text style={styles.addButtonText}>Add Gift</Text>
-      </TouchableOpacity>
     </View>
   );
 }
