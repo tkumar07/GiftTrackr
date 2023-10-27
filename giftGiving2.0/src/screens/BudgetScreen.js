b46ffd2b17e37c9c2e0c75a583dd16b57ac7f948
Shang-Hsun Yang <shanghsunyang@169-231-23-59.wireless.ucsb.edu>
2023-10-26 17:14:07 -0700

    User can input the gift data now. The gift information can be stored locally now.

--- giftGiving2.0/src/screens/BudgetScreen.js
+++ giftGiving2.0/src/screens/BudgetScreen.js
@@ -1,26 +1,21 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+import { useState } from "react";
+import { View, Text, TextInput } from "react-native";
+import { styles } from "../styles";
 
 function BudgetScreen() {
-  // 1. Initialize budget to 0
-  const [totalBudget, setTotalBudget] = useState(0);
-  const [amountSpent, setAmountSpent] = useState(0);  // assuming this might be used
-
-  // Dummy function to represent fetching gift data
-  const getGiftDataCost = () => {
-    // Placeholder value for now. In a real scenario, fetch the actual data.
-    return 50;  
-  };
-
-  const updateBudget = () => {
-    let giftCost = getGiftDataCost();
-    setTotalBudget(totalBudget - giftCost);
-  };
+  const [totalBudget, setTotalBudget] = useState(1000); // Default budget
+  const [amountSpent, setAmountSpent] = useState(0);
+  const amountRemaining = totalBudget - amountSpent;
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Budget Screen</Text>
-      <BudgetSection totalBudget={totalBudget} amountSpent={amountSpent} />
+      <View style={styles.container}>
+        <Text>Total Budget: ${totalBudget}</Text>
+        <Text>Amount Spent: ${amountSpent}</Text>
+        <Text>Amount Remaining: ${amountRemaining}</Text>
+      </View>
 
       <Text>Set Your Budget:</Text>
       <TextInput
@@ -29,23 +24,8 @@ function BudgetScreen() {
         onChangeText={(value) => setTotalBudget(Number(value))}
         keyboardType="numeric"
       />
-
-      {/* 4. Message if Budget is 0 */}
-      {totalBudget === 0 && <Text>Please set your budget!</Text>}
-
-      <Button title="Update Budget with Gift Data" onPress={updateBudget} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  input: {
-    width: "80%",
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-  },
-});
 
 export default BudgetScreen;
