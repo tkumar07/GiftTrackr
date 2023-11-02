992460efd2bef54d65c5ec015a6b6ee7b86fafe9
tkumar07 <72886906+tkumar07@users.noreply.github.com>
2023-11-01 20:03:44 -0700

    hidden password, hide/show button for login page

--- giftGiving2.0/src/screens/Login.js
+++ giftGiving2.0/src/screens/Login.js
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, Alert, Text } from "react-native";
-import { collection, query, where, getDocs } from "@firebase/firestore";
-import { db } from "../config/firebase";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { collection, query, where, getDocs } from '@firebase/firestore';
+import { db } from "../config/firebase"
+
+const PasswordInput = ({ password, setPassword, showPassword, toggleShowPassword }) => {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center', borderColor: 'gray', borderWidth: 1 }}>
+      <TextInput
+        style={{ flex: 1, height: 40 }}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Enter password"
+        secureTextEntry={!showPassword}
+      />
+      {password.length > 0 && (
+        <Button
+          title={showPassword ? "Hide" : "Show"}
+          onPress={toggleShowPassword}
+        />
+      )}
+    </View>
+  );
+};
 
 const Login = (props) => {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async () => {
     try {
@@ -16,7 +41,7 @@ const Login = (props) => {
         const userData = querySnapshot.docs[0].data();
         if (userData.password === password) {
           if (props && props.onSuccessfulLogin) {
-            console.log("HELLO THERE ", username);
+            console.log("HELLO THERE", username);
             props.onSuccessfulLogin(username);
           }
           console.log("Login successful");
@@ -33,19 +58,21 @@ const Login = (props) => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 16 }}>
-      <Text>LOGIN</Text>
+      <TextInput>LOGIN</TextInput>
       <TextInput
         style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
         onChangeText={setUsername}
         value={username}
         placeholder="Enter username"
       />
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Enter password"
+
+      <PasswordInput
+        password={password}
+        setPassword={setPassword}
+        showPassword={showPassword}
+        toggleShowPassword={toggleShowPassword}
       />
+
       <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
