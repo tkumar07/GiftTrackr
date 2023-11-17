61dac23043c3b068bbd527811d995520b3fe99fc
Tanish Kumar <tanish_kumar@ucsb.edu>
2023-11-16 17:09:52 -0800

    added cards for calendar/firebase integration

--- giftGiving2.0/src/screens/CalendarScreen.js
+++ giftGiving2.0/src/screens/CalendarScreen.js
@@ -1,71 +1,125 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Dimensions, TextInput } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import { Calendar } from "react-native-calendars";
 import moment from "moment";
 import { styles } from "../styles";
-import CustomButton from "../components/CustomButton";
+import { collection, query, where, doc, getDoc, getDocs } from "@firebase/firestore";
+import { db } from "../config/firebase";
+import { useFocusEffect } from "@react-navigation/native";
+import { SimplifiedCard } from "../components/SimplifiedCard";
+import { ScrollView } from "react-native";
 
-const CalendarScreen = () => {
+const CalendarScreen = (props) => {
+  const { username } = props.route.params;
   const [markedDates, setMarkedDates] = useState({});
   const [selectedDate, setSelectedDate] = useState(null);
-  const [eventNames, setEventNames] = useState({});
-  const [newEventName, setNewEventName] = useState("");
-  const dotColor = '#C6E9F7';
+  const [giftDetails, setGiftDetails] = useState([]);
+  
 
-  const events = {};
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchDates();
+    }, [props.route.params.username])
+  );
 
-  useEffect(() => {
-    const initialMarkedDates = {};
-    for (const date in events) {
-      initialMarkedDates[date] = {
-        selected: true,
-        marked: true,
-        dotColor,
-      };
-    }
-    setMarkedDates(initialMarkedDates);
-  }, []);
+  const fetchDates = async () => {
+    try {
+      const usersRef = collection(db, "users");
+      const userQuery = query(usersRef, where("username", "==", username));
+      const userQuerySnapshot = await getDocs(userQuery);
 
-  const handleDayPress = (day) => {
-    const date = day.dateString;
-    setSelectedDate(date);
-    setNewEventName(eventNames[date] || "");
-  };
+      if (!userQuerySnapshot.empty) {
+        const markedDatesData = {};
 
-  const handleAddEvent = () => {
-    if (selectedDate) {
-      const updatedEventNames = { ...eventNames };
-      updatedEventNames[selectedDate] = newEventName;
-      setEventNames(updatedEventNames);
+        for (const userDoc of userQuerySnapshot.docs) {
+          const userData = userDoc.data();
+          const userGiftIDs = userData.gifts || [];
 
-      // Show a small blue dot underneath the selected date
-      setMarkedDates((prevMarkedDates) => ({
-        ...prevMarkedDates,
-        [selectedDate]: {
-          ...prevMarkedDates[selectedDate],
-          selected: true,
-          marked: true,
-          dotColor,
-        },
-      }));
-    }
-  };
+          for (const giftID of userGiftIDs) {
+            const giftRef = doc(db, "gifts", giftID);
+            const giftDoc = await getDoc(giftRef);
 
-  const handleRemoveEvent = () => {
-    if (selectedDate) {
-      const updatedEventNames = { ...eventNames };
-      delete updatedEventNames[selectedDate];
-      setEventNames(updatedEventNames);
-
-      // Remove the dot from the selected date
-      setMarkedDates((prevMarkedDates) => {
-        const updatedMarkedDates = { ...prevMarkedDates };
-        if (updatedMarkedDates[selectedDate]) {
-          delete updatedMarkedDates[selectedDate];
+            if (giftDoc.exists()) {
+              const date = moment(giftDoc.data().date).format("YYYY-MM-DD");
+              markedDatesData[date] = {
+                selected: true,
+                marked: true,
+                dotColor: "#C6E9F7",
+              };
+            }
+          }
         }
-        return updatedMarkedDates;
-      });
+
+        setMarkedDates(markedDatesData);
+        console.log("Dates fetched successfully");
+      } else {
+        console.log("No gifts found");
+      }
+    } catch (error) {
+      console.error("Error fetching dates: ", error);
     }
+  };
+
+  const handleDayPress = async (day) => {
+    const date = moment(day.dateString).format("YYYY-MM-DD"); // Format consistently
+    setSelectedDate(date);
+  
+    try {
+      const usersRef = collection(db, "users");
+      const userQuery = query(usersRef, where("username", "==", username));
+      const userQuerySnapshot = await getDocs(userQuery);
+  
+      if (!userQuerySnapshot.empty) {
+        const selectedGiftDetails = [];
+  
+        for (const userDoc of userQuerySnapshot.docs) {
+          const userData = userDoc.data();
+          const userGiftIDs = userData.gifts || [];
+  
+          for (const giftID of userGiftIDs) {
+            const giftRef = doc(db, "gifts", giftID);
+            const giftDoc = await getDoc(giftRef);
+  
+            if (giftDoc.exists()) {
+              const giftDate = moment(giftDoc.data().date).format("YYYY-MM-DD");
+  
+              if (giftDate === date) {
+                selectedGiftDetails.push({
+                  recipient: giftDoc.data().recipient,
+                  date: giftDate,
+                  occasion: giftDoc.data().occasion,
+                });
+              }
+            }
+          }
+        }
+  
+        setGiftDetails(selectedGiftDetails);
+      }
+    } catch (error) {
+      console.error("Error fetching gift details: ", error);
+    }
+  };
+  
+  const renderGiftCards = () => {
+    return (
+      <View>
+        {giftDetails.map((gift, index) => (
+          <SimplifiedCard
+            key={index}
+            recipient={gift.recipient}
+            date={gift.date}
+            occasion={gift.occasion}
+            likes={gift.likes}
+            dislikes={gift.dislikes}
+          />
+        ))}
+      </View>
+    );
+  };
+
+  const formatDateForDisplay = (date) => {
+    return moment(date).format("MMMM D, YYYY");
   };
 
   const screenHeight = Dimensions.get("window").height;
@@ -80,84 +134,38 @@ const CalendarScreen = () => {
     >
       <Text style={styles.pageHeader}>Calendar</Text>
       <View style={{ paddingVertical: 0, width: "100%" }}>
-        <Calendar style={{
-          borderColor: '#C6E9F7',
-          backgroundColor: '#C6E9F7',
-        }}
-
-  markedDates={markedDates} onDayPress={handleDayPress} enableSwipeMonths={true} />
+        <Calendar
+          style={{
+            borderColor: "#C6E9F7",
+            backgroundColor: "#C6E9F7",
+          }}
+          markedDates={markedDates}
+          onDayPress={handleDayPress}
+          enableSwipeMonths={true}
+        />
       </View>
       {selectedDate && (
-        <View style={unique_styles.agendaContainer}>
+        <View style={unique_styles.dateContainer}>
           <Text style={unique_styles.dateText}>
-            {moment(selectedDate).format("MMMM D, YYYY")}
+            {formatDateForDisplay(selectedDate)}
           </Text>
-          {eventNames[selectedDate] && (
-            <Text style={unique_styles.eventName}>{eventNames[selectedDate]}</Text>
-          )}
-          {eventNames[selectedDate] ? (
-            <CustomButton title="Remove Event" onPress={handleRemoveEvent}/>
-          ) : (
-            <View>
-              <TextInput
-                style={styles.input}
-                placeholder="Event Name"
-                value={newEventName}
-                onChangeText={(text) => setNewEventName(text)}
-              />
-              <CustomButton title="Add Event" onPress={handleAddEvent}/>
-            </View>
-          )}
         </View>
+      )}
+      {selectedDate && giftDetails && (
+          <ScrollView>{renderGiftCards()}</ScrollView>
       )}
     </View>
   );
 };
 
 const unique_styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  pageHeader: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  calendarContainer: {
-    paddingVertical: 20,
-  },
-  agendaContainer: {
-    padding: 16,
+  dateContainer: {
+    alignItems: "center",
+    marginVertical: 10,
   },
   dateText: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
-  },
-  eventName: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  customButton: {
-    alignItems: "center",
-    padding: 10,
-    borderRadius: 5,
-    margin: 5,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-  },
-  eventInput: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 10,
-    padding: 8,
   },
 });
 
